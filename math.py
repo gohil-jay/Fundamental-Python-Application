c81d7ee0f0574a49e18639595bc739ba85e6d82c
gohil-jay <gohil.p.jay@gmail.com>
2020-10-26 10:18:57 +0530

    Added math script

---
+++ math.py
@@ -0,0 +1,24 @@
+import math
+
+print(math.pi) #Returns Pi's constant number value
+print(math.e) #Returns Euler's constant number value
+print(math.tau) #Returns Tau' constant number value
+
+print(math.radians(30)) #Return radian value
+print(math.degree((math.pi)/6)) #Return degree value
+
+print(math.sin(0.5)) #Returns sin's value
+print(math.cos(0.5)) #Returns cos' value
+
+print(math.log(10)) #Returens log value
+print(math.log10(10)) #Returns log base-10 value
+
+print(math.sqrt(100)) #Returns square root value
+
+print(math.exp(10)) #Returns exponent value
+
+print(math.pow(2,4)) #Returns power (2 raise to 4 here) value
+
+print(math.ceil(4.5867)) #Returns ceiling value
+print(math.floor(4.5867)) #Returns floor value
+
