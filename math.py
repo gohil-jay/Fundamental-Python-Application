e432cd4c32a3b21322387f6cb3887f807f0382ae
gohil-jay <gohil.p.jay@gmail.com>
2020-10-27 11:03:49 +0530

    Added more functions from math module

--- math.py
+++ math.py
@@ -9,8 +9,10 @@ print(math.degree((math.pi)/6)) #Return degree value
 
 print(math.sin(0.5)) #Returns sin's value
 print(math.cos(0.5)) #Returns cos' value
+print(math.tan(0.5)) #Returns tan's value
 
 print(math.log(10)) #Returens log value
+print(math.log2(10)) #Returns log base-2 value
 print(math.log10(10)) #Returns log base-10 value
 
 print(math.sqrt(100)) #Returns square root value
@@ -19,6 +21,14 @@ print(math.exp(10)) #Returns exponent value
 
 print(math.pow(2,4)) #Returns power (2 raise to 4 here) value
 
+print(math.remainder(9, 2))
+
 print(math.ceil(4.5867)) #Returns ceiling value
 print(math.floor(4.5867)) #Returns floor value
 
+print(math.exp(10)) #Returns exponent value
+
+print(math.pow(2,4)) #Returns power (2 raise to 4 here) value
+
+print(math.ceil(4.5867)) #Returns ceiling value
+print(math.floor(4.5867)) #Returns floor value
